7c580773b601d91384e0e066c97cb475d56475e5
Phillips-Peace <159626308+Phillips-Peace@users.noreply.github.com>
2024-04-23 13:54:17 +0100

    Add files via upload

---
+++ MY PORTFOLIO DATA.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{813F176C-4F5A-4731-B34B-E1FE20053B53}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {7090E4B8-8B4B-412C-B1BC-820FE2C56D10}
+	EndGlobalSection
+EndGlobal
